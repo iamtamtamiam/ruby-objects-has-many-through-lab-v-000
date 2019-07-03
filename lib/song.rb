17eb9a3c4946972534158057a6ccbb9a68f5a7a5
Tam Vu <vutam_a@hotmail.com>
2019-07-03 21:30:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = namelear
+    @artist = artist
+    @genre = genre 
+  end 
+  
+end 
